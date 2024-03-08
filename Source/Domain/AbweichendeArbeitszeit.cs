f86b49655697198b1b68fe7c8c78446a14105082
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2024-03-08 13:43:04 +0100

    feat(domain): add AbweichendeArbeitszeit

---
+++ Source/Domain/AbweichendeArbeitszeit.cs
@@ -0,0 +1,30 @@
+using CSharpFunctionalExtensions;
+
+namespace Perosnaldisposition;
+
+public class AbweichendeArbeitszeit
+{
+    public DayOfWeek Tag { get; }
+    public TimeOnly Beginn { get; }
+    public TimeOnly Ende { get; }
+
+    private AbweichendeArbeitszeit(DayOfWeek tag, TimeOnly beginn, TimeOnly ende)
+    {
+        Tag = tag;
+        Beginn = beginn;
+        Ende = ende;
+    }
+
+    public static Result<AbweichendeArbeitszeit> Create(DayOfWeek tag, TimeOnly beginn, TimeOnly ende)
+    {
+        if (tag is DayOfWeek.Saturday or DayOfWeek.Sunday)
+            return
+                Result.Failure<AbweichendeArbeitszeit>("Für das Wochenende kann keine Abweichende Arbeitszeit erzeugt werden");
+
+        if (ende < beginn)
+            return
+                Result.Failure<AbweichendeArbeitszeit>("Das Arbeitsende darf nicht vor dem Arbeitsbeginn liegen.");
+
+        return Result.Success(new AbweichendeArbeitszeit(tag, beginn, ende));
+    }
+}
---
+++ Source/Domain/AbweichendeArbeitszeitTests.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+
+namespace Perosnaldisposition;
+
+public class AbweichendeArbeitszeitTests
+{
+    [Theory]
+    [InlineData(DayOfWeek.Saturday)]
+    [InlineData(DayOfWeek.Sunday)]
+    public void Wenn_eine_abweichende_Arbeitszeit_am_Wochenende_erzeugt_werden_soll_schlägt_dies_fehl(
+        DayOfWeek dayOfWeekend)
+    {
+        var beginn = TimeOnly.FromDateTime(DateTime.Today.AddHours(10));
+        var ende = TimeOnly.FromDateTime(DateTime.Today.AddHours(9));
+
+        var abweichendeArbeitszeit = AbweichendeArbeitszeit.Create(dayOfWeekend, beginn, ende);
+
+        abweichendeArbeitszeit.Should().Fail();
+    }
+
+    [Fact]
+    public void Wenn_eine_abweichende_Arbeitszeit_erzeugt_wird_darf_das_Arbeitsende_nicht_vor_dem_Arbeitsbeginn_sein()
+    {
+        var beginn = TimeOnly.FromDateTime(DateTime.Today.AddHours(10));
+        var ende = TimeOnly.FromDateTime(DateTime.Today.AddHours(9));
+
+        var abweichendeArbeitszeit = AbweichendeArbeitszeit.Create(DayOfWeek.Monday, beginn, ende);
+
+        abweichendeArbeitszeit.Should().Fail();
+    }
+
+    [Fact]
+    public void Wenn_eine_abweichende_Arbeitszeit_Werktags_mit_korrekten_Zeitangaben_erzeugt_wird_gelingt_dies()
+    {
+        var beginn = TimeOnly.FromDateTime(DateTime.Today.AddHours(8));
+        var ende = TimeOnly.FromDateTime(DateTime.Today.AddHours(16));
+
+        var abweichendeArbeitszeit = AbweichendeArbeitszeit.Create(DayOfWeek.Monday, beginn, ende);
+
+        abweichendeArbeitszeit.Should().Succeed();
+
+        abweichendeArbeitszeit.Value.Tag.Should().Be(DayOfWeek.Monday);
+        abweichendeArbeitszeit.Value.Beginn.Should().Be(beginn);
+        abweichendeArbeitszeit.Value.Ende.Should().Be(ende);
+    }
+}
