a2ce9ca9e76bc593c79f17a2172d7df435ab7c0a
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2024-03-08 14:25:05 +0100

    feat(mitarbeiter): allow "Abweichende Arbeitszeitvereinbarung"

--- Source/Domain/Mitarbeiter.cs
+++ Source/Domain/Mitarbeiter.cs
@@ -1,46 +1,52 @@
-﻿using CSharpFunctionalExtensions;
+﻿using System.Collections.Immutable;
+using CSharpFunctionalExtensions;
 
 namespace Perosnaldisposition;
 
-public class Mitarbeiter: IEquatable<Mitarbeiter>
+public class Mitarbeiter : IEquatable<Mitarbeiter>
 {
-
-    public Guid Id { get; set; }
-    
-    public string Vorname { get; }
-
-    public string Nachname { get; }
-    
-    public Gruppe Gruppe { get; }
-
-    public IEnumerable<Taetigkeit> QualifizierteTaetigkeiten { get; private set; }
-    
     private Mitarbeiter(string vorname, string nachname, Gruppe gruppe)
     {
         Vorname = vorname;
         Nachname = nachname;
         Gruppe = gruppe;
 
+        AbweichendeArbeitszeiten = ImmutableDictionary<DayOfWeek, AbweichendeArbeitszeit>.Empty;
         QualifizierteTaetigkeiten = Array.Empty<Taetigkeit>();
     }
-    
+
+    public Guid Id { get; set; }
+
+    public string Vorname { get; }
+
+    public string Nachname { get; }
+
+    public Gruppe Gruppe { get; }
+
+    public ImmutableDictionary<DayOfWeek, AbweichendeArbeitszeit> AbweichendeArbeitszeiten { get; set; }
+    public IEnumerable<Taetigkeit> QualifizierteTaetigkeiten { get; private set; }
+
+
+    public bool Equals(Mitarbeiter? andererMitarbeiter)
+    {
+        if (ReferenceEquals(null, andererMitarbeiter)) return false;
+        if (ReferenceEquals(this, andererMitarbeiter)) return true;
+
+        return andererMitarbeiter?.Vorname == Vorname
+            && andererMitarbeiter?.Nachname == Nachname
+            && Gruppe.Equals(andererMitarbeiter.Gruppe);
+    }
+
     public static Result<Mitarbeiter> Create(string vorname, string nachname, Gruppe gruppe)
     {
         if (string.IsNullOrWhiteSpace(vorname))
-        {
             return Result.Failure<Mitarbeiter>("Bitte geben Sie den Vornamen des Mitarbeiters an.");
-        }
-        
-        if (string.IsNullOrWhiteSpace(nachname))
-        {
-            return Result.Failure<Mitarbeiter>("Bitte geben Sie den Nachnamen des Mitarbeiters an.");
-        }
 
-        if (gruppe is null)
-        {
-            return Result.Failure<Mitarbeiter>("Bitte weisen Sie dem Mitarbeiter eine Gruppe zu.");
-        }
-        
+        if (string.IsNullOrWhiteSpace(nachname))
+            return Result.Failure<Mitarbeiter>("Bitte geben Sie den Nachnamen des Mitarbeiters an.");
+
+        if (gruppe is null) return Result.Failure<Mitarbeiter>("Bitte weisen Sie dem Mitarbeiter eine Gruppe zu.");
+
         return Result.Success(new Mitarbeiter(vorname, nachname, gruppe));
     }
 
@@ -50,29 +56,28 @@ public class Mitarbeiter: IEquatable<Mitarbeiter>
             QualifizierteTaetigkeiten.Any(qualifizierteTaetigkeit => qualifizierteTaetigkeit.Name == taetigkeit.Name);
 
         if (istBereitsFuerTaetigkeitQualidizierung)
-        {
             return
                 Result.Failure($"Dem Mitarbeiter {Vorname} {Nachname} ist für die Tätigkeit {taetigkeit.Name} bereits qualifiziert");
-        }
-        
+
         QualifizierteTaetigkeiten = QualifizierteTaetigkeiten.Append(taetigkeit);
-        
+
         return Result.Success();
+    }
+
+
+    public void Vereinbare(AbweichendeArbeitszeit abweichendeArbeitszeit)
+    {
+        AbweichendeArbeitszeiten = AbweichendeArbeitszeiten.SetItem(abweichendeArbeitszeit.Tag, abweichendeArbeitszeit);
+    }
+
+    public void LoeseArbeitszeitVereinbarungFuer(DayOfWeek tag)
+    {
+        AbweichendeArbeitszeiten = AbweichendeArbeitszeiten.Remove(tag);
     }
 
     public bool IstQualifiziertFuer(Taetigkeit taetigkeit)
     {
         return QualifizierteTaetigkeiten.Any(q => q.Name == taetigkeit.Name);
-    }
-
-    public bool Equals(Mitarbeiter? andererMitarbeiter)
-    {
-        if (ReferenceEquals(null, andererMitarbeiter)) return false;
-        if (ReferenceEquals(this, andererMitarbeiter)) return true;
-        
-        return andererMitarbeiter?.Vorname == Vorname 
-            && andererMitarbeiter?.Nachname == Nachname 
-            && Gruppe.Equals(andererMitarbeiter.Gruppe);
     }
 
     public override bool Equals(object? obj)
--- Source/Domain/MitarbeiterTests.cs
+++ Source/Domain/MitarbeiterTests.cs
@@ -11,12 +11,12 @@ public class MitarbeiterTests
         var nachname = "";
         var gruppe = Gruppe.Create("Frühschicht");
 
-        
+
         var mitarbeiter = Mitarbeiter.Create(vorname, nachname, gruppe.Value);
 
         mitarbeiter.Should().Fail();
     }
-    
+
     [Fact]
     public void Wenn_ein_Mitarbeiter_ohne_Nachnamen_angelegt_wird_schlägt_dies_fehl()
     {
@@ -39,7 +39,7 @@ public class MitarbeiterTests
 
         mitarbeiter.Should().Fail();
     }
-    
+
     [Fact]
     public void Wenn_ein_Mitarbeiter_mit_allen_nötigen_Informationen_angelegt_wird_funktioniert_es()
     {
@@ -54,8 +54,8 @@ public class MitarbeiterTests
         mitarbeiter.Value.Nachname.Should().Be(nachname);
         mitarbeiter.Value.Gruppe.Name.Should().Be(gruppe.Value.Name);
     }
-    
-    
+
+
     [Fact]
     public void Wenn_ein_Mitarbeiter_angelegt_wurde_ist_er_für_keine_Tätigkeit_qualifiziert()
     {
@@ -67,14 +67,15 @@ public class MitarbeiterTests
 
         mitarbeiter.Value.QualifizierteTaetigkeiten.Should().BeEmpty();
     }
-    
- 
+
+
     [Fact]
-    public void Wenn_ein_Mitarbeiter_für_eine_Tätigkeit_qualifiziert_ist_kann_die_gleiche_Taetigkeit_nicht_noch_einmal_qualifiziert_werden()
+    public void
+        Wenn_ein_Mitarbeiter_für_eine_Tätigkeit_qualifiziert_ist_kann_die_gleiche_Taetigkeit_nicht_noch_einmal_qualifiziert_werden()
     {
         var vorname = "Alan";
         var nachname = "Turing";
-        
+
         var gruppe = Gruppe.Create("Frühschicht");
         var taetigkeit = Taetigkeit.Create("Einlagern");
 
@@ -103,7 +104,7 @@ public class MitarbeiterTests
 
         istDieSelbePerson.Should().BeTrue();
     }
-    
+
     [Fact]
     public void
         Wenn_zwei_Mitarbeiter_den_selben_Vornamen_und_Nachnamen_haben_aber_in_verschiedenen_Gruppen_arbeiten_handelt_es_sich_um_verschiedene_Mitarbeiter()
@@ -120,5 +121,69 @@ public class MitarbeiterTests
         var istDieSelbePerson = alan.Value.Equals(auchAlan.Value);
 
         istDieSelbePerson.Should().BeFalse();
+    }
+
+    [Fact]
+    public void
+        Wenn_ein_Mitarbeiter_an_einem_Tag_eine_von_seiner_Gruppe_abweichende_Arbeitszeit_möchte_kann_sie_vereinbart_werden()
+    {
+        var vorname = "Alan";
+        var nachname = "Turing";
+
+        var fruehschicht = Gruppe.Create("Frühschicht");
+
+        var alan = Mitarbeiter.Create(vorname, nachname, fruehschicht.Value);
+
+        var montagsVon8Bis14Uhr =
+            AbweichendeArbeitszeit.Create(DayOfWeek.Monday, TimeOnly.Parse("08:00:00"), TimeOnly.Parse("14:00:00"));
+
+        alan.Value.Vereinbare(montagsVon8Bis14Uhr.Value);
+
+        alan.Value.AbweichendeArbeitszeiten.Should().HaveCount(1);
+    }
+    
+    [Fact]
+    public void
+        Wenn_für_einen_Mitarbeiter_eine_abweichende_Arbeitszeit_vereinbart_wurde_und_diese_verändert_wird_gelingt_dies()
+    {
+        var vorname = "Alan";
+        var nachname = "Turing";
+
+        var fruehschicht = Gruppe.Create("Frühschicht");
+
+        var alan = Mitarbeiter.Create(vorname, nachname, fruehschicht.Value);
+
+        var montagsVon8Bis14Uhr =
+            AbweichendeArbeitszeit.Create(DayOfWeek.Monday, TimeOnly.Parse("08:00:00"), TimeOnly.Parse("14:00:00"));
+
+        var montagsVon6Bis12Uhr =
+            AbweichendeArbeitszeit.Create(DayOfWeek.Monday, TimeOnly.Parse("06:00:00"), TimeOnly.Parse("12:00:00"));
+
+        
+        alan.Value.Vereinbare(montagsVon8Bis14Uhr.Value);
+        alan.Value.Vereinbare(montagsVon6Bis12Uhr.Value);
+
+        alan.Value.AbweichendeArbeitszeiten[DayOfWeek.Monday].Beginn.Should().Be(TimeOnly.Parse("06:00:00"));
+    }
+    
+    [Fact]
+    public void
+        Wenn_für_einen_Mitarbeiter_eine_abweichende_Arbeitszeit_vereinbart_wurde_und_diese_gelöst_wird_gelingt_dies()
+    {
+        var vorname = "Alan";
+        var nachname = "Turing";
+
+        var fruehschicht = Gruppe.Create("Frühschicht");
+
+        var alan = Mitarbeiter.Create(vorname, nachname, fruehschicht.Value);
+
+        var montagsVon8Bis14Uhr =
+            AbweichendeArbeitszeit.Create(DayOfWeek.Monday, TimeOnly.Parse("08:00:00"), TimeOnly.Parse("14:00:00"));
+
+        alan.Value.Vereinbare(montagsVon8Bis14Uhr.Value);
+
+        alan.Value.LoeseArbeitszeitVereinbarungFuer(montagsVon8Bis14Uhr.Value.Tag);
+
+        alan.Value.AbweichendeArbeitszeiten.Should().BeEmpty();
     }
 }
